c3335450a65702932e1000dbebcf1f4097d5c693
Mark Edelman <markedelman1987@gmail.com>
2017-01-01 19:09:35 -0500

    error if num isn't a number

--- test/test-fizz-buzzer.js
+++ test/test-fizz-buzzer.js
@@ -1,3 +1,45 @@
 const should = require('chai').should();
 
 const adder = require('../fizzBuzzer');
+
+describe('fizzBuzzer', function() {
+
+
+
+    it('should throw error if num is not a number', function() {
+
+        const nonNumberInputs = [a, ' ', ''];
+
+        nonNumberInputs.forEach(function(input) {
+
+            fizzBuzzer(input).should.throw(Error);
+        });
+
+    });
+
+
+
+    it('should return fizz-buzz if num is divisible by 15', function() {
+
+
+    });
+
+
+    it('should return buzz if num is divisible by 5', function() {
+
+
+    });
+
+
+    it('should return fizz if num is divisible by 3', function() {
+
+
+    });
+
+
+    it('should return num if num is not divisible by 15,5,3', function() {
+
+
+    });
+
+});
